1488d812e85c952ea5fe7d519ac5c9dd10f42a4b
En Shih <seanstone5923@gmail.com>
2018-09-27 17:24:49 +0800

    Buildroot post-build - add removes

--- configs/post-build.sh
+++ configs/post-build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-rm -rf ${TARGET_DIR}/usr/include
-rm -rf ${TARGET_DIR}/usr/lib/cmake
+rm -rf ${TARGET_DIR}/usr/include/
+rm -rf ${TARGET_DIR}/usr/lib/cmake/
 rm -rf ${TARGET_DIR}/usr/lib/firmware/*
+rm -rf ${TARGET_DIR}/etc/wpa_supplicant/
