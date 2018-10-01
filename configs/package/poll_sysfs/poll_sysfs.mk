37cdd915445cd1581fe56cb855d01350d84bd49b
En Shih <seanstone5923@gmail.com>
2018-10-01 11:40:54 +0800

    Fix poll_sysfs paths

--- configs/package/poll_sysfs/poll_sysfs.mk
+++ configs/package/poll_sysfs/poll_sysfs.mk
@@ -5,11 +5,12 @@
 ################################################################################
 
 POLL_SYSFS_LICENSE = GPLv2
-
+POLL_SYSFS_SITE := $(BR2_EXTERNAL)/package/poll_sysfs
+POLL_SYSFS_SITE_METHOD := local
 
 define POLL_SYSFS_BUILD_CMDS
 	$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) \
-		package/poll_sysfs/poll_sysfs.c -o $(@D)/poll_sysfs
+		$(@D)/poll_sysfs.c -o $(@D)/poll_sysfs
 endef
 
 define POLL_SYSFS_INSTALL_TARGET_CMDS
