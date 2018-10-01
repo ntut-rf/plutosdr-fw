f9983e62a499efbf5087d6a1f3983b863de1528d
En Shih <seanstone5923@gmail.com>
2018-10-01 11:36:59 +0800

    Fix gr-iio dependencies

--- configs/package/gr-iio/gr-iio.mk
+++ configs/package/gr-iio/gr-iio.mk
@@ -1,6 +1,7 @@
 GR_IIO_VERSION := e9d12b5
 GR_IIO_SITE := https://github.com/analogdevicesinc/gr-iio.git
 GR_IIO_SITE_METHOD := git
+GR_IIO_DEPENDENCIES += libad9361-iio
 GR_IIO_INSTALL_TARGET := YES
 
 $(eval $(cmake-package))
