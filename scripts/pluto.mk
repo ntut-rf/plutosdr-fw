1432d9de7604569446e2804f8ed7cb1a8fa0c0b2
En Shih <seanstone5923@gmail.com>
2019-04-04 16:43:14 +0800

    Remove unused target parameters

--- scripts/pluto.mk
+++ scripts/pluto.mk
@@ -1,10 +1,7 @@
 
 # Target specific constants go here
 
-HDF_URL:=http://github.com/analogdevicesinc/plutosdr-fw/releases/download/${LATEST_TAG}/system_top.hdf
-TARGET_DTS_FILES:= zynq-pluto-sdr.dtb zynq-pluto-sdr-revb.dtb zynq-pluto-sdr-revc.dtb
 COMPLETE_NAME:=PlutoSDR
-ZIP_ARCHIVE_PREFIX:=plutosdr
 DEVICE_VID:=0x0456
 DEVICE_PID:=0xb673
 export HDL_PROJECT := pluto
