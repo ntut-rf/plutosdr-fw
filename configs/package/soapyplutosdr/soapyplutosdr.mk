1bf52c2dc7b5acaeee1898b46e3351fd22986511
En Shih <seanstone5923@gmail.com>
2019-04-05 00:44:13 +0800

    Add soapysdr pacakges

---
+++ configs/package/soapyplutosdr/soapyplutosdr.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# SoapyPlutoSDR
+#
+################################################################################
+
+SOAPYPLUTOSDR_VERSION = master
+SOAPYPLUTOSDR_SITE = https://github.com/jocover/SoapyPlutoSDR.git
+SOAPYPLUTOSDR_SITE_METHOD = git
+
+SOAPYPLUTOSDR_INSTALL_STAGING = YES
+SOAPYPLUTOSDR_LICENSE = LGPL-2.1+
+SOAPYPLUTOSDR_LICENSE_FILE = LICENSE
+SOAPYPLUTOSDR_DEPENDENCIES = libiio libad9361-iio soapysdr
+
+$(eval $(cmake-package))
