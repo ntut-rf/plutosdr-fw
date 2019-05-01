f2672f6922bf89a257c10a89a62e86a2ef5e37e3
En Shih <seanstone5923@gmail.com>
2019-05-01 19:16:11 +0800

    Add ADRV9364 hdl defines

---
+++ targets/adrv9364/hdl/system_bd.tcl
@@ -0,0 +1,2 @@
+
+source -notrace $ad_hdl_dir/projects/adrv9364z7020/ccbob_lvds/system_bd.tcl
---
+++ targets/adrv9364/hdl/system_project.tcl
@@ -0,0 +1,21 @@
+
+set ad_hdl_dir [file normalize $::env(ADI_HDL_DIR)]
+
+source -notrace $ad_hdl_dir/projects/scripts/adi_env.tcl
+source -notrace $ad_hdl_dir/projects/scripts/adi_project.tcl
+source -notrace $ad_hdl_dir/projects/scripts/adi_board.tcl
+
+set lib_dirs "$ad_hdl_dir/../build/ip"
+
+set p_device "xc7z020clg400-1"
+adi_project_xilinx adrv9364z7020_ccbob_lvds
+adi_project_files adrv9364z7020_ccbob_lvds [list \
+  "$ad_hdl_dir/library/xilinx/common/ad_iobuf.v" \
+  "$ad_hdl_dir/projects/common/adrv9364z7020_constr.xdc" \
+  "$ad_hdl_dir/projects/common/adrv9364z7020_constr_lvds.xdc" \
+  "$ad_hdl_dir/projects/common/ccbob_constr.xdc" \
+  "system_top.v" ]
+
+adi_project_run adrv9364z7020_ccbob_lvds
+source $ad_hdl_dir/library/axi_ad9361/axi_ad9361_delay.tcl
+
