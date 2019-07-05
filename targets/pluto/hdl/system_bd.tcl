4ea050568442aabe4988e6492cb0deca52b778c3
En Shih <seanstone5923@gmail.com>
2019-07-05 22:01:54 +0800

    Add example HLS IP

--- targets/pluto/hdl/system_bd.tcl
+++ targets/pluto/hdl/system_bd.tcl
@@ -6,7 +6,7 @@ source -notrace $ad_hdl_dir/projects/pluto/system_bd.tcl
 ad_ip_instance axi_simplereg axi_simplereg
 ad_cpu_interconnect 0x40000000 axi_simplereg
 
-# my_mult
+# add_one
 
-#ad_ip_instance my_mult_axis my_mult_axis_0
-#ad_cpu_interconnect 0x41000000 my_mult_axis_0
+ad_ip_instance add_one add_one
+ad_cpu_interconnect 0x41000000 add_one
