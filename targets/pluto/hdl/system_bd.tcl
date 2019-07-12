d549eaa9cd1c469858938012f67f5acb7c09baf2
En Shih <seanstone5923@gmail.com>
2019-07-12 10:22:59 +0800

    Add axi-stream data fifo

--- targets/pluto/hdl/system_bd.tcl
+++ targets/pluto/hdl/system_bd.tcl
@@ -3,10 +3,31 @@ source -notrace $ad_hdl_dir/projects/pluto/system_bd.tcl
 
 # gpreg
 
-ad_ip_instance axi_simplereg axi_simplereg
-ad_cpu_interconnect 0x40000000 axi_simplereg
+ad_ip_instance axi_simplereg axi_simplereg_0
+ad_cpu_interconnect 0x40000000 axi_simplereg_0
 
-# add_one
+# add_one (lite)
 
-ad_ip_instance lite_add_one lite_add_one
-ad_cpu_interconnect 0x41000000 lite_add_one
+ad_ip_instance lite_add_one lite_add_one_0
+ad_cpu_interconnect 0x41000000 lite_add_one_0
+
+# add_one (stream)
+
+ad_ip_instance axi_dma axi_dma_0
+#ad_ip_parameter axi_dma_0 CONFIG.PCW_UIPARAM_DDR_BOARD_DELAY1 0.240
+ad_cpu_interconnect 0x42000000 axi_dma_0
+ad_connect sys_cpu_clk axi_dma_0/m_axi_sg_aclk
+ad_connect sys_cpu_clk axi_dma_0/m_axi_mm2s_aclk
+ad_connect sys_cpu_clk axi_dma_0/m_axi_s2mm_aclk
+ad_mem_hp0_interconnect sys_cpu_clk axi_dma_0/M_AXI_SG
+ad_mem_hp0_interconnect sys_cpu_clk axi_dma_0/M_AXI_SG
+ad_mem_hp0_interconnect sys_cpu_clk axi_dma_0/M_AXI_MM2S
+ad_mem_hp0_interconnect sys_cpu_clk axi_dma_0/M_AXI_S2MM
+
+ad_ip_instance axis_data_fifo axis_data_fifo_0
+ad_connect sys_cpu_clk axis_data_fifo_0/s_axis_aclk
+ad_connect sys_cpu_resetn axis_data_fifo_0/s_axis_aresetn
+ad_connect axi_dma_0/S_AXIS_S2MM axis_data_fifo_0/M_AXIS
+ad_connect axi_dma_0/M_AXIS_MM2S axis_data_fifo_0/S_AXIS
+
+
