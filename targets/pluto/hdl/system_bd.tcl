5c56711c5976c67e8809e4d3352a023c28d98f7c
En Shih <seanstone5923@gmail.com>
2019-07-18 00:52:57 +0800

    Testing axi stream hls ip

--- targets/pluto/hdl/system_bd.tcl
+++ targets/pluto/hdl/system_bd.tcl
@@ -8,8 +8,8 @@ ad_cpu_interconnect 0x40000000 axi_simplereg_0
 
 # add_one (lite)
 
-ad_ip_instance lite_add_one lite_add_one_0
-ad_cpu_interconnect 0x41000000 lite_add_one_0
+ad_ip_instance lite_add_one axil_add_one_0
+ad_cpu_interconnect 0x41000000 axil_add_one_0
 
 # add_one (stream)
 
@@ -24,13 +24,17 @@ ad_mem_hp0_interconnect sys_cpu_clk axi_dma_0/M_AXI_SG
 ad_mem_hp0_interconnect sys_cpu_clk axi_dma_0/M_AXI_SG
 ad_mem_hp0_interconnect sys_cpu_clk axi_dma_0/M_AXI_MM2S
 ad_mem_hp0_interconnect sys_cpu_clk axi_dma_0/M_AXI_S2MM
-
-ad_ip_instance axis_data_fifo axis_data_fifo_0
-ad_connect sys_cpu_clk axis_data_fifo_0/s_axis_aclk
-ad_connect sys_cpu_resetn axis_data_fifo_0/s_axis_aresetn
-ad_connect axi_dma_0/S_AXIS_S2MM axis_data_fifo_0/M_AXIS
-ad_connect axi_dma_0/M_AXIS_MM2S axis_data_fifo_0/S_AXIS
-
 ad_cpu_interrupt ps-0 mb-0 axi_dma_0/mm2s_introut
 ad_cpu_interrupt ps-1 mb-1 axi_dma_0/s2mm_introut
 
+# ad_ip_instance axis_data_fifo axis_data_fifo_0
+# ad_connect sys_cpu_clk axis_data_fifo_0/s_axis_aclk
+# ad_connect sys_cpu_resetn axis_data_fifo_0/s_axis_aresetn
+# ad_connect axi_dma_0/M_AXIS_MM2S axis_data_fifo_0/S_AXIS
+# ad_connect axi_dma_0/S_AXIS_S2MM axis_data_fifo_0/M_AXIS
+
+ad_ip_instance stream_add_one axis_add_one_0
+ad_connect sys_cpu_clk axis_add_one_0/ap_clk
+ad_connect sys_cpu_resetn axis_add_one_0/ap_rst_n
+ad_connect axi_dma_0/M_AXIS_MM2S axis_add_one_0/a
+ad_connect axi_dma_0/S_AXIS_S2MM axis_add_one_0/b
