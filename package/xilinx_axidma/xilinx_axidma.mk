5a66fa6c11470f59ca602652d1d37e9914796350
En Shih <seanstone5923@gmail.com>
2019-07-17 22:37:15 +0800

    Add  xilinx_axidma package

---
+++ package/xilinx_axidma/xilinx_axidma.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+# xilinx_axidma
+#
+################################################################################
+
+XILINX_AXIDMA_VERSION := 42ed91e
+XILINX_AXIDMA_SITE := https://github.com/bperez77/xilinx_axidma.git
+XILINX_AXIDMA_SITE_METHOD := git
+XILINX_AXIDMA_DEPENDENCIES +=
+XILINX_AXIDMA_INSTALL_TARGET := YES
+
+#XILINX_AXIDMA_MODULE_SUBDIRS = driver
+
+define XILINX_AXIDMA_BUILD_CMDS
+	cd $(@D) && $(MAKE) $(TARGET_CONFIGURE_OPTS) CFLAGS+="-Wno-error" CROSS_COMPILE=arm-buildroot-linux-gnueabihf- ARCH=arm KBUILD_DIR=$(LINUX_DIR) driver
+	cd $(@D) && $(MAKE) $(TARGET_CONFIGURE_OPTS) CFLAGS+="-Wno-error" CROSS_COMPILE=arm-buildroot-linux-gnueabihf- ARCH=arm examples library
+endef
+
+define XILINX_AXIDMA_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/outputs/*.ko $(TARGET_DIR)/usr/lib/modules/
+	$(INSTALL) -D $(@D)/outputs/*.so $(TARGET_DIR)/usr/lib/
+	$(INSTALL) -D -m 755 $(@D)/outputs/{axidma_benchmark,axidma_display_image,axidma_transfer} $(TARGET_DIR)/usr/bin/
+endef
+
+#$(eval $(kernel-module))
+$(eval $(generic-package))
