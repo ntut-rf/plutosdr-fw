65f43b2577296a7abf97cdca1db7334c30910b3a
En Shih <seanstone5923@gmail.com>
2019-08-07 09:10:40 +0800

    pluto: system_bd remove simplereg

--- targets/pluto/hdl/system_bd.tcl
+++ targets/pluto/hdl/system_bd.tcl
@@ -3,8 +3,8 @@ source -notrace $ad_hdl_dir/projects/pluto/system_bd.tcl
 
 ## gpreg
 
-ad_ip_instance axi_simplereg axi_simplereg_0
-ad_cpu_interconnect 0x40000000 axi_simplereg_0
+# ad_ip_instance axi_simplereg axi_simplereg_0
+# ad_cpu_interconnect 0x40000000 axi_simplereg_0
 
 ## add_one (lite)
 
