0442ff7364582603b971c8adfa8a51f009dfd786
En Shih <seanstone5923@gmail.com>
2019-08-07 23:21:04 +0800

    xilinx_axidma package: add linux dependency

--- package/xilinx_axidma/xilinx_axidma.mk
+++ package/xilinx_axidma/xilinx_axidma.mk
@@ -8,6 +8,7 @@ XILINX_AXIDMA_VERSION := 42ed91e
 XILINX_AXIDMA_SITE := https://github.com/bperez77/xilinx_axidma.git
 XILINX_AXIDMA_SITE_METHOD := git
 XILINX_AXIDMA_INSTALL_TARGET := YES
+XILINX_AXIDMA_DEPENDENCIES := linux
 
 define XILINX_AXIDMA_BUILD_CMDS
 	cd $(@D) && $(MAKE) $(TARGET_CONFIGURE_OPTS) CFLAGS+="-Wno-error" CROSS_COMPILE=arm-buildroot-linux-gnueabihf- ARCH=arm KBUILD_DIR=$(LINUX_DIR) driver
