7c05395a52a6a5bf70dbffc869591d1ab13b182d
En Shih <seanstone5923@gmail.com>
2019-09-27 17:31:00 +0800

    Move gnuradio files

--- ip/dvbt_reed_solomon_enc/dvbt_reed_solomon_enc_ip.tcl
+++ ip/dvbt_reed_solomon_enc/dvbt_reed_solomon_enc_ip.tcl
@@ -5,8 +5,7 @@ open_project ${ip_name}.prj
 set_top ${ip_name}
 
 ## Add Files
-add_files "encode_rs.cc init_rs.cc" -cflags "-I."
-add_files "${ip_name}_impl.cc" -cflags "-I. -std=gnu++0x"
+add_files "${ip_name}_impl.cc encode_rs.cc init_rs.cc" -cflags "-I.. -std=gnu++0x"
 
 ## Add Test Bench Files
 add_files -tb ${ip_name}_tb.cpp
