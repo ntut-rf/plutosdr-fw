252eb51ab753449a751699bf7126a2b36265726f
En Shih <seanstone5923@gmail.com>
2020-01-22 21:30:45 +0800

    Fix gr-iio depedencies

--- package/gr-iio/gr-iio.mk
+++ package/gr-iio/gr-iio.mk
@@ -1,7 +1,7 @@
 GR_IIO_VERSION := 8196f3d
 GR_IIO_SITE := https://github.com/analogdevicesinc/gr-iio.git
 GR_IIO_SITE_METHOD := git
-GR_IIO_DEPENDENCIES += libad9361-iio
+GR_IIO_DEPENDENCIES += libad9361-iio libiio boost gnuradio
 GR_IIO_INSTALL_TARGET := YES
 
 $(eval $(cmake-package))
