0cdde0e869b3ac19fea18e974220baac4882dfad
En Shih <seanstone5923@gmail.com>
2020-02-04 19:39:29 +0800

    Add sse2neon package

---
+++ package/sse2neon/sse2neon.mk
@@ -0,0 +1,11 @@
+SSE2NEON_VERSION := 65cdf9a
+SSE2NEON_SITE := https://github.com/DLTcollab/sse2neon.git
+SSE2NEON_SITE_METHOD := git
+SSE2NEON_DEPENDENCIES +=
+SSE2NEON_INSTALL_STAGING := YES
+
+define SSE2NEON_INSTALL_STAGING_CMDS
+	$(INSTALL) -D $(@D)/*.h $(STAGING_DIR)/usr/include/
+endef
+
+$(eval $(generic-package))
