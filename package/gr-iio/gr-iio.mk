f1fefef94058347729c29a0bf1e3ff4158c49452
En Shih <seanstone5923@gmail.com>
2020-02-07 01:05:42 +0800

    Fix gr-iio

--- package/gr-iio/gr-iio.mk
+++ package/gr-iio/gr-iio.mk
@@ -1,5 +1,5 @@
-GR_IIO_VERSION := 8196f3d
-GR_IIO_SITE := https://github.com/analogdevicesinc/gr-iio.git
+GR_IIO_VERSION := 7a5046f
+GR_IIO_SITE := https://github.com/seanstone/gr-iio.git
 GR_IIO_SITE_METHOD := git
 GR_IIO_DEPENDENCIES += libad9361-iio libiio boost gnuradio
 GR_IIO_INSTALL_TARGET := YES
