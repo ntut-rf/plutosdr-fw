3661ad914ea52fefef40dd4b3697d239cd5cc8ff
En Shih <seanstone5923@gmail.com>
2020-02-08 01:27:11 +0800

    Create multimon-ng package

---
+++ package/multimon-ng/multimon-ng.mk
@@ -0,0 +1,7 @@
+MULTIMON_NG_VERSION := 84947ce
+MULTIMON_NG_SITE := https://github.com/EliasOenal/multimon-ng.git
+MULTIMON_NG_SITE_METHOD := git
+MULTIMON_NG_DEPENDENCIES += 
+MULTIMON_NG_INSTALL_TARGET := YES
+
+$(eval $(cmake-package))
