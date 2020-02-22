3964bee39dbc7342061e2513ad3d4952417a1daf
En Shih <seanstone5923@gmail.com>
2020-02-22 16:34:06 +0800

    Revert "Fixing xilinx_axidma package WIP"
    This reverts commit c62535c56d009bdd8d294e0fb59637fcfb3ad68e.

--- package/xilinx_axidma/xilinx_axidma.mk
+++ package/xilinx_axidma/xilinx_axidma.mk
@@ -10,18 +10,16 @@ XILINX_AXIDMA_SITE_METHOD := git
 XILINX_AXIDMA_INSTALL_TARGET := YES
 XILINX_AXIDMA_DEPENDENCIES := linux
 
-XILINX_AXIDMA_MODULE_SUBDIRS = driver
-
-export AXIDMA_MODULE_NAME = axidma
-export AXIDMA_FILES = axi_dma.c axidma_chrdev.c axidma_dma.c axidma.h axidma_of.c
-export AXIDMA_INC_DIRS = $(@D)/include
-$(eval $(kernel-module))
-
 define XILINX_AXIDMA_BUILD_CMDS
-	$(MAKE) -C $(@D) $(TARGET_CONFIGURE_OPTS) CFLAGS+="-Wno-error" CROSS_COMPILE=arm-buildroot-linux-gnueabihf- ARCH=arm examples library
+	cd $(@D) && $(MAKE) $(TARGET_CONFIGURE_OPTS) CFLAGS+="-Wno-error" CROSS_COMPILE=arm-buildroot-linux-gnueabihf- ARCH=arm KBUILD_DIR=$(LINUX_DIR) driver
+	cd $(@D) && $(MAKE) $(TARGET_CONFIGURE_OPTS) CFLAGS+="-Wno-error" CROSS_COMPILE=arm-buildroot-linux-gnueabihf- ARCH=arm examples library
 endef
 
+MODULES_DEP = $(TARGET_DIR)/usr/lib/modules/$(LINUX_VERSION_PROBED)/modules.dep
+
 define XILINX_AXIDMA_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/outputs/*.ko $(TARGET_DIR)/usr/lib/modules/$(LINUX_VERSION_PROBED)/kernel/
+	grep -qxF 'kernel/axidma.ko:' $(MODULES_DEP) || echo 'kernel/axidma.ko:' >> $(MODULES_DEP)
 	$(INSTALL) -D $(@D)/outputs/*.so $(TARGET_DIR)/usr/lib/
 	$(INSTALL) -D -m 755 $(@D)/outputs/{axidma_benchmark,axidma_display_image,axidma_transfer} $(TARGET_DIR)/usr/bin/
 endef
