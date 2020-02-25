484d5d1bbc943c98bda2374900ae9a25ede969c4
En Shih <seanstone5923@gmail.com>
2020-02-25 09:53:30 +0800

    Fix adrv9364 build

--- targets/adrv9364/hdl/system_project.tcl
+++ targets/adrv9364/hdl/system_project.tcl
@@ -2,13 +2,14 @@
 set ad_hdl_dir [file normalize $::env(ADI_HDL_DIR)]
 
 source -notrace $ad_hdl_dir/projects/scripts/adi_env.tcl
-source -notrace $ad_hdl_dir/projects/scripts/adi_project.tcl
+source -notrace $ad_hdl_dir/projects/scripts/adi_project_xilinx.tcl
 source -notrace $ad_hdl_dir/projects/scripts/adi_board.tcl
 
 set lib_dirs "$ad_hdl_dir/../build/ip"
 
 set p_device "xc7z020clg400-1"
-adi_project_xilinx adrv9364z7020_ccbob_lvds
+adi_project adrv9364z7020_ccbob_lvds
+
 adi_project_files adrv9364z7020_ccbob_lvds [list \
   "$ad_hdl_dir/library/xilinx/common/ad_iobuf.v" \
   "$ad_hdl_dir/projects/adrv9364z7020/common/adrv9364z7020_constr.xdc" \
