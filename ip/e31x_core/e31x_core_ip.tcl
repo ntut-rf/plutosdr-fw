a537f70128a858e20a3169bee9092602027d41c9
En Shih <seanstone5923@gmail.com>
2020-03-04 19:02:17 +0800

    Add e31x_core ip sources

--- ip/e31x_core/e31x_core_ip.tcl
+++ ip/e31x_core/e31x_core_ip.tcl
@@ -15,6 +15,9 @@ adi_ip_files $library_name [split $library_src " "]
 #set_property verilog_define [split $verilog_defs " "] [current_fileset]
 set_property verilog_define {GIT_HASH=32'h0c75f3d0 RFNOC_EDGE_TBL_FILE=/home/en/plutosdr-fw/ettus-fpga/usrp3/top/e31x/e310_static_router.hex} [current_fileset]
 
+report_ip_status
+upgrade_ip [get_ips *]
+
 adi_ip_properties_lite $library_name
 
 ipx::save_core [ipx::current_core]
