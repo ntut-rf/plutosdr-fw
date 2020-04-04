aad44d2e3688cec5125729e0430ea90a3347a9a3
En Shih <seanstone5923@gmail.com>
2020-04-04 11:00:56 +0800

    Update SISO build commands

--- package/SISO/SISO.mk
+++ package/SISO/SISO.mk
@@ -12,12 +12,10 @@ SISO_INSTALL_TARGET := YES
 define SISO_BUILD_CMDS
 	$(MAKE) -C $(@D)/app clean
 	$(MAKE) WORKING_DIR=$(@D) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" LD="$(TARGET_LD)" -C $(@D)/app
-	$(MAKE) WORKING_DIR=$(@D) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" LD="$(TARGET_LD)" -C $(@D) top_block.py
 endef
 
 define SISO_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/app/bin/* $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -D -m 0755 $(@D)/top_block.py $(TARGET_DIR)/usr/bin/
 endef
 
 $(eval $(generic-package))
