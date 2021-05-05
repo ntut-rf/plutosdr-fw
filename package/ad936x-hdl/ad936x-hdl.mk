4534bc1e9f5f99c9bb31ada245f4418e3add4916
En Shih <seanstone5923@gmail.com>
2021-05-05 17:45:24 +0800

    create_fsbl_project in ad936x-hdl package

--- package/ad936x-hdl/ad936x-hdl.mk
+++ package/ad936x-hdl/ad936x-hdl.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-AD936X_HDL_SITE_METHOD = local
-AD936X_HDL_SITE = $(BR2_EXTERNAL)/hdl
+AD936X_HDL_SITE_METHOD = git
+AD936X_HDL_SITE = https://github.com/analogdevicesinc/hdl
+AD936X_HDL_VERSION = e773b22
 
 export HDL_PROJECT
 export HDL_PROJECT_DIR = $(@D)/projects/$(HDL_PROJECT)
@@ -17,6 +18,7 @@ export ADI_USE_OOC_SYNTHESIS = 1
 define AD936X_HDL_BUILD_CMDS
 	cp -v $(BR2_EXTERNAL)/targets/$(TARGET)/hdl/*.tcl $(HDL_PROJECT_DIR)
 	source $(VIVADO_SETTINGS) && $(MAKE) -j1 VPATH=$(HDL_PROJECT_DIR) -I $(HDL_PROJECT_DIR) -C $(HDL_PROJECT_DIR)
+	source $(VIVADO_SETTINGS) && cd $(O) && xsdk -batch -source $(BR2_EXTERNAL)/package/ad936x-hdl/create_fsbl_project.tcl
 endef
 
 $(eval $(generic-package))
