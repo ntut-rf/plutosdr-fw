dcc634d70b3decb2f0b8d1301d0c33fc7275aaa9
En Shih <seanstone5923@gmail.com>
2021-05-07 14:07:13 +0800

    Remove legacy files

--- targets/adrv9364/adrv9364.mk
+++
@@ -1,4 +0,0 @@
-export HDL_PROJECT := adrv9364z7020_ccbob_lvds
-export HDL_PROJECT_DIR := $(CURDIR)/hdl/projects/adrv9364z7020/ccbob_lvds/
-
-FSBL_LOAD_BITSTREAM = 1
--- targets/pluto/pluto.mk
+++
@@ -1,2 +0,0 @@
-export HDL_PROJECT := pluto
-export HDL_PROJECT_DIR := $(CURDIR)/hdl/projects/pluto
