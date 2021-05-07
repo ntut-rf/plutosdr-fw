5bfbe79bf17a134a3bc04f2e57e4ed361a83bf22
En Shih <seanstone5923@gmail.com>
2021-05-07 14:05:13 +0800

    Update ad936x hdl

--- package/ad936x-hdl/ad936x-hdl.mk
+++ package/ad936x-hdl/ad936x-hdl.mk
@@ -6,13 +6,15 @@
 
 AD936X_HDL_SITE_METHOD = git
 AD936X_HDL_SITE = https://github.com/analogdevicesinc/hdl
-AD936X_HDL_VERSION = e773b22
+AD936X_HDL_VERSION = 1db04a4
 
 HDL_PROJECT_DIR = $(@D)/projects/$(HDL_PROJECT)
 
 export MODE = incr
 export ADI_USE_OOC_SYNTHESIS = 1
 export ADI_HDL_DIR = $(@D)
+
+export ADI_IGNORE_VERSION_CHECK = 1
 
 define AD936X_HDL_BUILD_CMDS
 	cp -v $(BR2_EXTERNAL)/targets/$(TARGET)/hdl/*.tcl $(HDL_PROJECT_DIR)
