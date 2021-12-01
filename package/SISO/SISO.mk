cbf05323877011765f079bdadd46fe509e1ed706
En Shih <seanstone5923@gmail.com>
2021-12-01 21:41:33 +0800

    Update SISO package

--- package/SISO/SISO.mk
+++ package/SISO/SISO.mk
@@ -5,16 +5,19 @@
 ################################################################################
 
 SISO_SITE_METHOD := local
-SISO_SITE := ~/SISO
+SISO_SITE := $(BR2_EXTERNAL)/../SISO
 SISO_DEPENDENCIES += gnuradio sse2neon libiio xilinx_axidma
 SISO_INSTALL_TARGET := YES
 
+export GNURADIO_VERSION
+
 define SISO_BUILD_CMDS
 	$(MAKE) -C $(@D) clean
-	$(MAKE) WORKING_DIR=$(@D) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" LD="$(TARGET_LD)" -C $(@D) apps
+	$(MAKE) WORKING_DIR=$(@D) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" LD="$(TARGET_LD)" -C $(@D) iq-apps iio-apps
 endef
 
 define SISO_INSTALL_TARGET_CMDS
+	rm -f $(@D)/bin/*.d
 	$(INSTALL) -D -m 0755 $(@D)/bin/* $(TARGET_DIR)/usr/bin/
 endef
 
