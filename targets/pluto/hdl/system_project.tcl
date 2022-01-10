87517b0d95b11748bcd604d82b503a416c4c02a0
En Shih <seanstone5923@gmail.com>
2022-01-10 15:18:09 +0800

    Disable axi_ad9361_delay.tcl as a workaround

--- targets/pluto/hdl/system_project.tcl
+++ targets/pluto/hdl/system_project.tcl
@@ -18,4 +18,6 @@ adi_project_files pluto [list \
 
 set_property is_enabled false [get_files  *system_sys_ps7_0.xdc]
 adi_project_run pluto
-source -notrace $ad_hdl_dir/library/axi_ad9361/axi_ad9361_delay.tcl
+
+# FIXME
+#source -notrace $ad_hdl_dir/library/axi_ad9361/axi_ad9361_delay.tcl
