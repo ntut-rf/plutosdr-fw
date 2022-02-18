e08518a0b698841481c44c8b9d3c8a5bd62a5c6d
En Shih <seanstone5923@gmail.com>
2022-02-18 15:08:33 +0800

    Add second FFT core to ADRV9364 hdl design

--- targets/adrv9364/hdl/system_bd.tcl
+++ targets/adrv9364/hdl/system_bd.tcl
@@ -59,4 +59,48 @@ ad_cpu_interconnect 0x41000000 axi_gpio_0
 ad_ip_parameter axi_gpio_0 CONFIG.C_GPIO_WIDTH 16
 ad_connect axi_gpio_0/gpio_io_o xfft_0/s_axis_config_tdata
 
+## AXI DMA 1
+
+ad_ip_instance axi_dma axi_dma_1
+ad_ip_parameter axi_dma_1 CONFIG.c_sg_include_stscntrl_strm 0
+ad_ip_parameter axi_dma_1 CONFIG.c_sg_length_width 23
+ad_cpu_interconnect 0x44000000 axi_dma_1
+ad_connect sys_cpu_clk axi_dma_1/m_axi_sg_aclk
+ad_connect sys_cpu_clk axi_dma_1/m_axi_mm2s_aclk
+ad_connect sys_cpu_clk axi_dma_1/m_axi_s2mm_aclk
+ad_mem_hp0_interconnect sys_cpu_clk axi_dma_1/M_AXI_SG
+ad_mem_hp0_interconnect sys_cpu_clk axi_dma_1/M_AXI_MM2S
+ad_mem_hp0_interconnect sys_cpu_clk axi_dma_1/M_AXI_S2MM
+ad_connect sys_ps7/FCLK_CLK0 axi_hp0_interconnect/S03_ACLK
+ad_connect sys_ps7/FCLK_CLK0 axi_hp0_interconnect/S04_ACLK
+ad_connect sys_ps7/FCLK_CLK0 axi_hp0_interconnect/S05_ACLK
+ad_connect sys_cpu_resetn axi_hp0_interconnect/S03_ARESETN
+ad_connect sys_cpu_resetn axi_hp0_interconnect/S04_ARESETN
+ad_connect sys_cpu_resetn axi_hp0_interconnect/S05_ARESETN
+ad_cpu_interrupt ps-2 mb-2 axi_dma_1/mm2s_introut
+ad_cpu_interrupt ps-3 mb-3 axi_dma_1/s2mm_introut
+
+## FFT 1
+
+ad_ip_parameter axi_dma_1 CONFIG.c_m_axis_mm2s_tdata_width 32
+
+ad_ip_instance xfft xfft_1
+ad_connect axi_dma_1/M_AXIS_MM2S xfft_1/S_AXIS_DATA
+ad_connect axi_dma_1/S_AXIS_S2MM xfft_1/M_AXIS_DATA
+ad_ip_parameter xfft_1 CONFIG.implementation_options radix_4_burst_io
+ad_ip_parameter xfft_1 CONFIG.transform_length 2048
+ad_ip_parameter xfft_1 CONFIG.output_ordering natural_order
+ad_ip_parameter xfft_1 CONFIG.rounding_modes convergent_rounding
+ad_ip_parameter xfft_1 CONFIG.complex_mult_type use_mults_performance
+#ad_ip_parameter xfft_1 CONFIG.butterfly_type use_xtremedsp_slices
+ad_connect sys_cpu_clk xfft_1/aclk
+
+ad_ip_instance xlconstant xlconstant_1
+ad_connect xlconstant_1/dout xfft_1/s_axis_config_tvalid
+
+ad_ip_instance axi_gpio axi_gpio_1
+ad_cpu_interconnect 0x43000000 axi_gpio_1
+ad_ip_parameter axi_gpio_1 CONFIG.C_GPIO_WIDTH 16
+ad_connect axi_gpio_1/gpio_io_o xfft_1/s_axis_config_tdata
+
 assign_bd_address
