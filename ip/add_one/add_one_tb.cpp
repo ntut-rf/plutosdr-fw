274847d6d148ca550a38a8568e5a2bf549597fd3
En Shih <seanstone5923@gmail.com>
2022-03-18 18:51:55 +0800

    Test Complex float data type for add_one IP

--- ip/add_one/add_one_tb.cpp
+++ ip/add_one/add_one_tb.cpp
@@ -4,16 +4,14 @@
 #include "add_one.h"
 
 #define INPUT_SIZE 100
-axis_uint8_t signal_in[INPUT_SIZE];
-axis_uint8_t signal_out[INPUT_SIZE];
+_Complex float signal_in[INPUT_SIZE];
+_Complex float signal_out[INPUT_SIZE];
 
 int main (void)
 {
 	for (int i=0; i<INPUT_SIZE; i++)
 	{
-		signal_in[i].data = i;
-		signal_in[i].user = 0x00;
-		signal_in[i].last = (i == INPUT_SIZE-1)? 1:0;
+		signal_in[i] = i;
 	}
 
 	//Perform top function:
@@ -22,7 +20,7 @@ int main (void)
 	//Check results...
 	for (int i=0; i<INPUT_SIZE; i++)
 	{
-		std::cout << std::hex << signal_out[i].data << std::endl;
+		std::cout << __real__ signal_out[i] << std::endl;
 	}
 
 	return 0;
